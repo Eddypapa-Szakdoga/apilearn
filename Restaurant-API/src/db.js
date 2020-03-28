a896a47cff022139dfc22d5377114f9345198300
Adam Toth <eddypapa@adams-mbp.home>
2020-03-28 13:50:50 +0100

    Create mongodb

--- Restaurant-API/src/db.js
+++ Restaurant-API/src/db.js
@@ -2,6 +2,17 @@ import mongoose from 'mongoose';
 import config from './config';
 
 export default callback => {
-    let db = mongoose.connect(config.mongoUrl);
+    //let db = mongoose.connect(config.mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+
+    let db = mongoose.connect(config.mongoUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('successfully connected to the database');
+    }).catch(err => {
+        console.log('error connecting to the database');
+        process.exit();
+    });
+
     callback(db);
 }
